2ab5e84fc44c385a9047eddc664fee801f705f93
PC-201705191109\Administrator <2099260054@qq.com>
2017-09-08 17:42:58 +0800

    从数据库中获取数据

---
+++ WebAPIDemo/WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<tblEmployee> Get()
+        {
+            using (SampleEntities entities = new SampleEntities())
+            {
+                return entities.tblEmployees.ToList();
+            }
+        }
+
+        public tblEmployee Get(int id)
+        {
+            using (SampleEntities entities = new SampleEntities())
+            {
+                return entities.tblEmployees.FirstOrDefault(e=>e.Id == id);
+            }
+        }
+    }
+}
